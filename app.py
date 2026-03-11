6aee3ded7f8f20462768422d7f1fc3852a891a8b
Guilherme Grandim <gui.grandim@gmail.com>
2026-03-11 13:35:23 -0300

    Main function added

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import streamlit as st
 def load_data():
     return pd.read_csv('data/processed/bikes_completed.csv')
 
-df = load_data()
+def main():
+    df = load_data()
+    
+    st.dataframe()
 
-st.dataframe(dt)
+if __name__ == '__main__':
+    main()

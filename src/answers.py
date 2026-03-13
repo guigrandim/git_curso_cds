07b1963e86ae16846c130af6887e93cb6e6834cf
Guilherme Grandim <gui.grandim@gmail.com>
2026-03-13 11:55:49 -0300

    answered question 9, 13, 14 from the first round of questions

--- src/answers.py
+++ src/answers.py
@@ -1,6 +1,63 @@
-##Teste de Aula do Git para restauação e recuperação de arquivo
+import pandas as pd
+import plotly.express as px
+import streamlit as st
 
-lista = [1, 2, 3]
+def rd1_question_9(df):
+    df_grouped = df[['id', 'seller_type']].groupby('seller_type')
 
-for i in lista:
-    print(i)
+    df_grouped = df_grouped.count().reset_index()
+
+    df_grouped = df_grouped.rename(columns={'id': 'count'})
+
+    df_grouped
+    
+    fig = px.bar(
+        df_grouped,
+        x="seller_type",
+        y="count",
+        labels={"sellers_type": "Seller Type", "count":"Quantity"}
+    )
+    
+    fig.update_traces(textposition="outside")
+    
+    st.plotly_chart(fig, use_container_width=True)
+    
+    return None
+
+def rd1_question13(df):
+    df_grouped = (
+        df.groupby("owner")
+        .agg(qty =pd.NamedAgg("id","count"))
+        .sort_values("qty")
+        .reset_index()
+    )
+    
+    fig = px.bar (
+        df_grouped,
+        x="owner",
+        y="qty",
+        labels={"owner": "Owner Types", "qty": "Quantuty"},
+        color = "owner",
+        text = "qty"
+    )
+    
+    fig.update_traces(textposition="outside")
+    
+    st.plotly_chart(fig, use_container_width = True)
+    
+    return None
+
+def rd1_question14(df):
+    st.text("As we can see, bikes with high kilometer have cheapier prices")
+    
+    fig = px.scatter(
+        df,
+        x="km_driven",
+        y="selling_price",
+        labels = {"km_driven": "Kilometers", "selling_price": "Selling Price"},
+    )
+    
+    st.plotly_chart(fig, use_container_width = True)
+    
+    return None
+
